7c82dd86705f48a7f45b53b7cc50052fed034bf5
richard <richard@nowhere.com>
2022-04-07 11:36:50 +1000

    basic console output

--- GameOfLifeApp/Program.cs
+++ GameOfLifeApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using GameOfLifeLib;
 using System;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GameOfLifeApp
@@ -13,16 +14,34 @@ namespace GameOfLifeApp
         /// <param name="y">vertical size. Default is 5.</param>
         public static async Task<int> Main(string[] args)
         {
+            const int edgeLength = 20;
+            const int maxTicks = 1000000;
+            int currentTick = 0;
+            int liveCells = -1;
+            Task nextTick;
             foreach (string arg in args)
             {
                 Console.Out.WriteLine("Got arg " + arg);
             }
-            GameOfLife g = new GameOfLife(5, 5);
+            GameOfLife g = new GameOfLife(edgeLength, edgeLength);
             g.setState(2, 1).setState(2, 2).setState(2, 3);
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
             Console.Out.Write(renderWorld(g.render()));
-            await Task.Run(() => { g.run(); });
-            Console.Out.Write(renderWorld(g.render()));
-            Console.Out.Write(renderWorld(g.run().render()));
+            nextTick = Task.Run(() => { g.run(); });
+            Console.SetCursorPosition(0, 0);
+            while (currentTick < maxTicks)
+            {
+                Console.Out.Flush();
+                await nextTick;
+                Console.Out.Write(renderWorld(g.render()));
+                //liveCells = g.GetLiveCount();
+                nextTick = Task.Run(() => { g.run(); });
+                Console.WriteLine(liveCells);
+                Console.SetCursorPosition(0, 0);
+                currentTick++;
+            }
+            Console.Out.Flush();
             return 0;
         }
 
